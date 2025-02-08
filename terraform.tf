c9e53ec542aa54ad2a3a94ef6608ce1444b4406f
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-02-08 11:20:16 +0800

    Add Terraform script

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  required_version = ">= 1.10.5"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "sctp-ce8-tfstate"
+    key    = "jsstrn-mod32-terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
