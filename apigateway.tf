9864d39d5f97b5753bcbb1c63060108e7e698138
linusgoh81 <linusgoh@gmail.com>
2025-02-08 12:17:55 +0800

    2nd commit for apigateway.tf

--- apigateway.tf
+++ apigateway.tf
@@ -1,4 +1,16 @@
-resource "aws_route53_record" "www" {}
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.your_zone.id # Replace with your Route53 zone ID
+  name    = "group2-urlshortener"         # Or "api" if you want api.group2-urlshortener.sctp-sandbox.com
+  type    = "ALIAS"
+  ttl     = "300" # Example TTL
+
+  alias {
+    name                   = aws_api_gateway_domain_name.shortener.regional_domain_name # Important!
+    zone_id                = aws_api_gateway_domain_name.shortener.regional_zone_id     # Important!
+    evaluate_target_health = true
+  }
+}
+
 
 resource "aws_api_gateway_domain_name" "shortener" {
   domain_name              = "group2-urlshortener.sctp-sandbox.com"
